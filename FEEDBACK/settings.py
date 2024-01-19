ab76b7651af69525fddefda8f9be48697041b17b
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2024-01-19 21:11:46 +0100

    second commit

--- FEEDBACK/settings.py
+++ FEEDBACK/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profiles',
     'reviews',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
+MEDIA_ROOT = BASE_DIR / "uploads"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
